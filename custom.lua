b15a17b2001096dc7265c6d4625286eede284d48
Your Name <you@example.com>
2016-09-19 23:15:06 +0200

    d

--- custom.lua
+++ custom.lua
@@ -10,7 +10,48 @@ local function switch_to_wired ()
     sys (configlua.cmds.switch_to_wired)
 end
 
+local function kill_us ()
+    info 'Killing us'
+--sys 's /etc/init.d/mpd restart'
+    sys 's ka fish-pines'
+end
+
+local function sndi ()
+    info 'sndi'
+    sys 'sndi'
+    kill_us ()
+end
+
+local function sndx ()
+    info 'sndx'
+    sys 'sndx'
+    kill_us ()
+end
+
+local rules_replace = nil
+
+local rules_enhance = {
+    music = {
+        press = {
+            { 'start',         once = true, handler = function () print 'extra' end },
+        },
+        release = {
+        },
+    },
+    general = {
+        press = {
+            { 'b',          once = true, handler = switch_to_wired },
+            { 'a',          once = true, handler = switch_to_wireless },
+            { 'down',       once = true, handler = kill_us },
+            { 'left',       once = true, handler = sndi },
+            { 'right',      once = true, handler = sndx },
+        },
+        release = {
+        },
+    },
+}
+
 return {
-    switch_to_wireless = switch_to_wireless,
-    switch_to_wired = switch_to_wired,
+    rules_replace = rules_replace,
+    rules_enhance = rules_enhance,
 }
