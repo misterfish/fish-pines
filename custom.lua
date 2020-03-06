d8f620a0f86e073f6784af922ded058e3a7f9f40
misterfish <fritz@babyelephant>
2020-03-06 21:53:50 +0100

    Whitespace

--- custom.lua
+++ custom.lua
@@ -1,11 +1,11 @@
 needs ({ me = 'custom' }, '__imported_util')
 
-local function switch_to_wireless () 
+local function switch_to_wireless ()
     info 'Switching to internet wired'
     sys (configlua.cmds.switch_to_wireless)
 end
 
-local function switch_to_wired () 
+local function switch_to_wired ()
     info 'Switching to internet wired'
     sys (configlua.cmds.switch_to_wired)
 end
