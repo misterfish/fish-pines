b43f67196fbe486dd48d7efa5ec7fb64cde65302
misterfish <fritz@babyelephant>
2020-03-08 19:09:22 +0100

    Remove save/

--- save/led.h
+++
@@ -1,5 +0,0 @@
-bool led_update_mode();
-bool led_update_remake_playlist_start();
-bool led_update_remake_playlist_stop();
-bool led_init();
-bool led_update_random(bool random);
--- save/mode.h
+++
@@ -1,7 +0,0 @@
-#include <stdbool.h>
-
-bool mode_general();
-bool mode_music();
-bool mode_set_music();
-bool mode_set_general();
-bool mode_toggle();
